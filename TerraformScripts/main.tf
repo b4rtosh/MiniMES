fd997797365993f5f8492bf4a161513198d0a8fd
Bartosh <b4rtosh@proton.me>
2024-08-22 17:51:18 +0200

    Updated main.tf

--- TerraformScripts/main.tf
+++ TerraformScripts/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_storage_blob" "dockercomposeblob" {
     storage_account_name   = azurerm_storage_account.sa.name
     storage_container_name = azurerm_storage_container.dockercompose.name
     type                   = "Block"
-    source                 = "./docker-compose.yaml"
+    source                 = "${path.module}/docker-compose.yaml"
 }
 
 resource "azurerm_app_service" "appservice" {
@@ -62,59 +62,3 @@ resource "azurerm_app_service" "appservice" {
         DOCKER_REGISTRY_SERVER_PASSWORD     = var.dockerhub_password
     }
 }
-
-
-# resource "azurerm_resource_group" "rg" {
-#   name = "minimesResourceGroup"
-#   location = "polandcentral"
-# }
-# 
-# # Create a container group for the Azure Container Instances
-# resource "azurerm_container_group" "containers" {
-#   name = "minimesContainerGroup"
-#   location = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   os_type = "Linux"
-#   ip_address_type = "Public"
-#   
-#   container {
-#     name = "minimes-server"
-#     image = "b4rtosh/minimes:server"
-#     cpu    = 0.5
-#     memory = 1.5
-#     ports {
-#       port = 5001
-#       protocol = "TCP"
-#     }
-#   }
-#   
-#   container {
-#     name = "minimes-front"
-#     image = "b4rtosh/minimes:front"
-#     cpu = 0.5
-#     memory = 1.5
-#     ports {
-#       port = 5174
-#       protocol = "TCP"
-#     }
-#   }
-#   
-#   container {
-#     name = "sql-server"
-#     image = "b4rtosh/minimes:sql"
-#     cpu = 1.0
-#     memory = 2
-#     environment_variables = {
-#       ACCEPT_EULA = "Y"
-#       SA_PASSWORD = "zaq1@WSX"
-#     }
-#     ports {
-#       port = 1433
-#       protocol = "TCP"
-#     }
-#   }
-#   
-#   tags = {
-#     environment = "production"
-#   }
-# }
