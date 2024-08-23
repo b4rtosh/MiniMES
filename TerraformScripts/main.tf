4e70cf74497f19c9f95aa22704e64be90948ce77
Bartosh <b4rtosh@proton.me>
2024-08-23 21:57:35 +0200

    Updated main.tf

--- TerraformScripts/main.tf
+++ TerraformScripts/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_app_service_plan" "appserviceplan" {
 }
 # Create storage account for the docker compose file
 resource "azurerm_storage_account" "sa" {
-    name                     = "dockercomposesa"
+    name                     = "dockerstorageacc"
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
